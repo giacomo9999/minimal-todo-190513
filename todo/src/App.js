cd91f4d33c332348b84eb3ca43bc439703fbe41d
James Gary <gmarchesi888@gmail.com>
2019-05-13 17:11:38 -0400

    added basic functionality to Todo component.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Todo from "./Todo";
 
-function App() {
-  return (
-    <div className="App">
-      
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+  }
+
+  render() {
+    return (
+      <div className="App">
         <h1>To Do App</h1>
         <Todo />
-      
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -1,11 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Todo extends Component {
-    render(){
-        return (
-            <h1>Todo Component here.</h1>
-        )
-    }
+  state = {
+    edit: false,
+    id: null,
+    mockData: [
+      {
+        id: "1",
+        title: "Buy Milk",
+        done: false,
+        date: new Date()
+      },
+      {
+        id: "2",
+        title: "Take Out Trash",
+        done: false,
+        date: new Date()
+      },
+      {
+        id: "3",
+        title: "Meet With Parole Officer",
+        done: false,
+        date: new Date()
+      },
+      {
+        id: "4",
+        title: "Pay Bills",
+        done: false,
+        date: new Date()
+      }
+    ]
+  };
+
+  onDeleteHandle(e) {
+    e.preventDefault();
+  }
+
+  onEditHandle(e) {
+    e.preventDefault();
+  }
+
+  onCompleteHandle(e) {
+    e.preventDefault();
+  }
+
+  onSubmitHandle(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmitHandle.bind(this)}>
+          <input type="text" name="item" className="item" />
+          <button className="btn-add-item">Add</button>
+        </form>
+        <ul>
+          {this.state.mockData.map(item => (
+            <li key={item.id}>
+              {item.title}
+              <button onClick={this.onDeleteHandle.bind(this, item.id)}>
+                Delete
+              </button>
+              <button onClick={this.onEditHandle.bind(this, item.id)}>
+                Edit
+              </button>
+              <button onClick={this.onCompleteHandle.bind(this, item.id)}>
+                Complete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Todo;
